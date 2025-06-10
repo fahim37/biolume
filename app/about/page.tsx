c45cc29d475fd3b40429a4793c32ac54abd9dab7
amitchackroborty04 <amitck803@gmail.com>
2025-06-10 15:36:10 +0600

    done about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import About_Section from './_component/About_Section'
 const page = () => {
   return (
     <div>
-          <Sub_banner vedieoLink="/asset/about_banner.mp4" title="About Us" description=""/>
+          <Sub_banner vedieoLink="/asset/about_banner1.mp4" title="About Us" description=""/>
           <About_Section/>
     </div>
   )
