21becfdacfa6f1bb9440a4262cc92728cf63780d
Fahim <ahmed.fahim37@gmail.com>
2025-06-11 18:13:47 +0600

    animations added

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,8 +1,10 @@
+"use client";
 import Hero from "@/components/web/Hero";
 import React from "react";
 import Service_solution from "./_component/Service_solution";
 import Image from "next/image";
 import Solution_Service from "./_component/Solution_Service";
+import { Particles } from "@/components/magicui/particles";
 
 const page = () => {
   return (
@@ -14,14 +16,15 @@ const page = () => {
       />
       <Solution_Service />
       <Service_solution />
-      <div className="py-5 bg-white">
-        {/* <Image
-          src="/asset/cllient_bottom.png"
-          alt="clients"
-          width={10000}
-          height={10000}
-          className="w-full h-full"
-        /> */}
+
+      <div className="relative overflow-hidden h-[150px] w-full">
+        <Particles
+          className="absolute inset-0 z-0"
+          quantity={700}
+          ease={80}
+          color={"#155FFF"}
+          refresh
+        />
       </div>
     </div>
   );
